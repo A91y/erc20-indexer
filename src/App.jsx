d40eae1dcc142571c07ac30889064f9e67b815d5
A91y <ayush.agr254@gmail.com>
2024-06-14 15:57:11 +0530

    chore: Refactor App component to use Chakra UI and Alchemy SDK

--- src/App.jsx
+++ src/App.jsx
@@ -8,48 +8,68 @@ import {
   Input,
   SimpleGrid,
   Text,
-} from '@chakra-ui/react';
-import { Alchemy, Network, Utils } from 'alchemy-sdk';
-import { useState } from 'react';
+} from "@chakra-ui/react";
+import { Alchemy, Network, Utils } from "alchemy-sdk";
+import { useState } from "react";
 
 function App() {
-  const [userAddress, setUserAddress] = useState('');
+  const [userAddress, setUserAddress] = useState("");
   const [results, setResults] = useState([]);
   const [hasQueried, setHasQueried] = useState(false);
   const [tokenDataObjects, setTokenDataObjects] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   async function getTokenBalance() {
-    const env = await import.meta.env;
-    const config = {
-      apiKey:   (env.VITE_ALCHEMY_API),
-      network: Network.ETH_MAINNET,
-    };
-
-    const alchemy = new Alchemy(config);
-    const data = await alchemy.core.getTokenBalances(userAddress);
-
-    setResults(data);
-
-    const tokenDataPromises = [];
-
-    for (let i = 0; i < data.tokenBalances.length; i++) {
-      const tokenData = alchemy.core.getTokenMetadata(
-        data.tokenBalances[i].contractAddress
-      );
-      tokenDataPromises.push(tokenData);
+    if (!userAddress.match(/^0x[a-fA-F0-9]{40}$/)) {
+      try {
+        const ensName = providers.resolveName(userAddress);
+        setUserAddress(ensName);
+      } catch (error) {
+        alert("Please enter a valid ethereum address");
+        return;
+      }
     }
+    try {
+      const env = await import.meta.env;
+      const config = {
+        apiKey: env.VITE_ALCHEMY_API,
+        network: Network.ETH_MAINNET,
+      };
 
-    setTokenDataObjects(await Promise.all(tokenDataPromises));
-    setHasQueried(true);
+      const alchemy = new Alchemy(config);
+      const data = await alchemy.core.getTokenBalances(userAddress);
+
+      setResults(data);
+      setLoading(true);
+
+      const tokenDataPromises = [];
+
+      for (let i = 0; i < data.tokenBalances.length; i++) {
+        const tokenData = alchemy.core.getTokenMetadata(
+          data.tokenBalances[i].contractAddress
+        );
+        tokenDataPromises.push(tokenData);
+      }
+
+      setTokenDataObjects(await Promise.all(tokenDataPromises));
+      setHasQueried(true);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError("Error: ", error);
+      setHasQueried(false);
+      console.log(error);
+      setError("Error: ", error);
+    }
   }
   return (
-    
     <Box w="100vw">
       <Center>
         <Flex
-          alignItems={'center'}
+          alignItems={"center"}
           justifyContent="center"
-          flexDirection={'column'}
+          flexDirection={"column"}
         >
           <Heading mb={0} fontSize={36}>
             ERC-20 Token Indexer
@@ -64,7 +84,7 @@ function App() {
         w="100%"
         flexDirection="column"
         alignItems="center"
-        justifyContent={'center'}
+        justifyContent={"center"}
       >
         <Heading mt={42}>
           Get all the ERC-20 token balances of this address:
@@ -74,27 +94,40 @@ function App() {
           color="black"
           w="600px"
           textAlign="center"
+          borderRadius={10}
           p={4}
           bgColor="white"
           fontSize={24}
         />
-        <Button fontSize={20} onClick={getTokenBalance} mt={36} bgColor="#rococo">
+        {error && (
+          <Text color="red" fontSize="sm">
+            {error}
+          </Text>
+        )}
+        <Button
+          fontSize={20}
+          onClick={getTokenBalance}
+          mt={36}
+          bgColor="#rococo"
+        >
           Check ERC-20 Token Balances
         </Button>
 
         <Heading my={36}>ERC-20 token balances:</Heading>
 
-        {hasQueried ? (
-          <SimpleGrid w={'90vw'} columns={4} spacing={24}>
+        {loading ? (
+          <p>Loading...</p>
+        ) : hasQueried ? (
+          <SimpleGrid w={"90vw"} columns={4} spacing={24}>
             {results.tokenBalances.map((e, i) => {
               return (
                 <Flex
-                  flexDir={'column'}
+                  flexDir={"column"}
                   color="white"
                   bg="transparent"
-                  w={'20vw'}
+                  w={"20vw"}
                   key={e.id}
-                  border={'1px solid #000001'}
+                  border={"1px solid #000001"}
                   p={10}
                 >
                   <Box>
@@ -113,7 +146,7 @@ function App() {
             })}
           </SimpleGrid>
         ) : (
-          'Please make a query! This may take a few seconds...'
+          "Please make a query! This may take a few seconds..."
         )}
       </Flex>
     </Box>
